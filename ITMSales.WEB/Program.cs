3a1aafb41e7920ef55875aafa027d994c5f905ac
Oscar Agudelo <oscar.agudelo@globant.com>
2023-02-22 10:20:38 -0500

    add CRUD in frontEnd

--- ITMSales.WEB/Program.cs
+++ ITMSales.WEB/Program.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ITMSales.WEB;
+using ITMSales.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7162/") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
 
